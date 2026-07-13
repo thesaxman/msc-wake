54d02538c1b9eefe0c34d9f71f907a525c9b9050
Ali <assassinsoflogic@outlook.com>
2026-07-13 19:56:51 +0100

    added dpi change

--- outputs.py
+++ outputs.py
@@ -4,8 +4,8 @@ from pathlib import Path
 OUT_DIR = Path("outputs")
 OUT_DIR.mkdir(exist_ok=True)
 
-def save_video(animation,filename):
+def save_video(animation,filename, dpi = 120):
     filepath = OUT_DIR / filename
-    animation.save(str(filepath), writer='ffmpeg', dpi=200, fps=20)
+    animation.save(str(filepath), writer='ffmpeg', dpi=dpi, fps=20)
     assert filepath.exists(), f"save produced no file: {filepath}"
     os.startfile(filepath)
