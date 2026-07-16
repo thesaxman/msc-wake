7edb12661c16cc8a78c8626a2768eb9cce24c9c3
Ali <assassinsoflogic@outlook.com>
2026-07-16 14:08:26 +0100

    changed normalising variable to delta_u1_0

--- multi_turbine_diff.py
+++ multi_turbine_diff.py
@@ -13,6 +13,8 @@ from video_utils import save_video
 from multi_forcing import u1_xt as u1_mf, u2_xt as u2_mf, yc_xt as yc_mf
 from super_position import u1_xt as u1_sp, u2_xt as u2_sp, yc_xt as yc_sp
 
+from shapiro_unsteady import delta_u1_0
+DELTA_U1_0 = delta_u1_0(wake_params)
 u1_xt = u1_mf - u1_sp
 u2_xt = u2_mf - u2_sp
 yc_xt = yc_mf - yc_sp
@@ -27,9 +29,9 @@ all_frames = jax.vmap(build_frame, in_axes=(None,0,0,None, None), out_axes=0)(x_
 assert all_frames.shape == (u1_xt.shape[0], y_grid.size, x_grid.size), all_frames.shape
 import numpy as np
 frames = np.asarray(all_frames)
-u1_xt = np.asarray(u1_xt)/wake_params.UINF
-u2_xt = np.asarray(u2_xt)/wake_params.UINF
-yc_xt = np.asarray(yc_xt)
+u1_xt = np.asarray(u1_xt)/DELTA_U1_0
+u2_xt = np.asarray(u2_xt)/DELTA_U1_0
+yc_xt = np.asarray(yc_xt)/yc_sp.max()
 fig, axes = plt.subplots(4, 1, figsize=(10, 12), sharex=True, layout= 'constrained', gridspec_kw={'height_ratios': [2.5, 1, 1, 1]})
 ax_field, ax_u1, ax_u2, ax_yc = axes
 
@@ -45,7 +47,7 @@ for ax in axes: # introduce gridlines to make figures
 
 mesh = ax_field.pcolormesh(x_grid/wake_params.D, y_grid/wake_params.D, frames[0], cmap='plasma',
                     shading='auto', vmin=0, vmax=frames.max())
-fig.colorbar(mesh, ax=ax_field, location = 'top', shrink = 0.5, label = r'$u_1/U_\infty$')
+fig.colorbar(mesh, ax=ax_field, location = 'top', shrink = 0.5, label = r'$u_1/\delta u_{{1,0}}^(0)$')
 cl_line, = ax_field.plot(x_grid/wake_params.D, yc_xt[0]/wake_params.D, 'w--', lw=1.2, label = r'$\Delta y_c$')
 ax_field.set_ylabel(r'$y/D$')
 ax_field.legend(loc='upper right')
@@ -54,17 +56,17 @@ ax_field.legend(loc='upper right')
 
 u1_line, = ax_u1.plot(x_grid/wake_params.D, u1_xt[0], lw=1.2)
 ax_u1.set_ylim(u1_xt.min(), 1.1*u1_xt.max())
-ax_u1.set_ylabel(r'$\Delta u_1/U_\infty$')
+ax_u1.set_ylabel(r'$\Delta u_1/\delta u_{{1,0}}^(0)$')
 
 
 u2_line, = ax_u2.plot(x_grid/wake_params.D, u2_xt[0], lw=1.2)
 ax_u2.set_ylim(1.1*u2_xt.min(), 1.1*u2_xt.max())
-ax_u2.set_ylabel(r'$\Delta u_2/U_\infty$')
+ax_u2.set_ylabel(r'$\Delta u_2/\delta u_{{1,0}}^(0)$')
 
 
 yc_line, = ax_yc.plot(x_grid/wake_params.D, yc_xt[0], lw=1.2)
 ax_yc.set_ylim(1.1*yc_xt.min(), 1.1*yc_xt.max())
-ax_yc.set_ylabel(r'$\Delta y_c$ [m]')
+ax_yc.set_ylabel(r'$\Delta y_c/y_{c,max}$ [m]')
 ax_yc.set_xlabel(r'$x/D$')
 
 timestamp = ax_field.text(0.02, 0.92, '', transform = ax_field.transAxes, color='w')
