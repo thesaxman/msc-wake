0efe33f9c50e3163fea2f183b4c14f91ac9012de
Ali <assassinsoflogic@outlook.com>
2026-07-21 23:08:05 +0100

    big refactoring of varying_turbine incorporating the essentials of unsteady_flow_solver and wake_dynamics

--- wake_dynamics.py
+++ wake_dynamics.py
@@ -32,6 +32,8 @@ class WakeParams(eqx.Module):
     boundary_diams: float = 20.0
     upstream_diams: float = -4.0
     gamma_fn: Callable = eqx.field(static = True, default = constant_gamma)
+    max_gamma_deg: float = 30.0
+    
     
     @property
     def sigma0(self):
@@ -82,10 +84,10 @@ def gaussian(x, yc, y, p: WakeParams):
 def u_point(x, yc, u1, y, p: WakeParams):
     return u1 * gaussian(x, yc, y, p)
 
-gamma_amplitude_rad = jnp.radians(30)
-gamma_frequency = 0.01827665508523  # Frequency of the sinusoidal variation in Hz
+MAX_YAW = float(jnp.radians(30)) # Max operating yaw taken as 30 degrees
+YAW_FREQUENCY = 8.333e-4  # NREL nominal yaw-rate normalised by full rotation for frequency
 
-def sinusoidal_variation(t, x0, amplitude, frequency):
+def sinusoidal_variation_t(t, x0, amplitude, frequency):
     """
     Function to output a sinusoidally time-varying quantity x about mean x = 0.
     
@@ -101,17 +103,19 @@ def sinusoidal_variation(t, x0, amplitude, frequency):
         
     Returns:
     float
-        Time-varying value.
+        Time-varying value resolved at t.
     """
     offset = jnp.arcsin(x0 / amplitude) / (2 * jnp.pi * frequency)
     return amplitude * jnp.sin(2 * jnp.pi * frequency * (t + offset))
 
-def gamma_t(t, gamma0):
-    """Time varying function of the yaw angle.
+def sinusoid_gamma_t(t, gamma0, amp = MAX_YAW, freq = YAW_FREQUENCY):
+    """Time varying sinusoidal function of the yaw angle.
 
     Args:
         t (Float): time variable
-        gamma0 (Float): initial yaw angle
+        gamma0 (Float): initial yaw angle (rad)
+        gamma_amp (float): yaw variation amplitude (rad)
+        gamma_freq (float): yaw variation frequency (Hz)
     """
     
-    return sinusoidal_variation(t, gamma0, amplitude=gamma_amplitude_rad, frequency=gamma_frequency)
+    return sinusoidal_variation_t(t, gamma0, amplitude=amp, frequency=freq)
